4142b575424cd0316bd93d39e3dac9ab06c83365
Lemontree <296995175@qq.com>
2017-05-28 15:43:22 +0800

    Initial Commit

---
+++ ThreeCountdown/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ThreeCountdown
+//
+//  Created by 抬头看见柠檬树 on 2017/5/28.
+//  Copyright © 2017年 抬头看见柠檬树. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ThreeCountdown/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThreeCountdown
+//
+//  Created by 抬头看见柠檬树 on 2017/5/28.
+//  Copyright © 2017年 抬头看见柠檬树. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
